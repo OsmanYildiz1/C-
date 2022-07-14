f54e284ca3e78a00411de72068190cea3eb62f4b
Osman Yıldız <67626189+OsmanYildiz1@users.noreply.github.com>
2022-07-15 02:26:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace sayısalişlemler
+{
+    class Program
+    {
+        static void Main(string[] args)   
+        {
+            int birinci = 34;
+            int ikinci = 29;
+            Console.WriteLine(birinci+ikinci); // 24+39
+            Console.WriteLine(birinci + ikinci + "string"); // topla yanına stringi yaz.
+            Console.WriteLine(birinci + "string" + ikinci); // yan yana yaz.
+            Console.WriteLine(birinci + "string" + ikinci + 10 ); // 34 string 39 olmalı. hayır 34 string 2910 oldu.
+            Console.WriteLine( (birinci +10 )+ "string" + ikinci + 10); // 44 string 2910 olmalı. doğru
+            // parantez içinde olan işlem yapıldı, parantez içindeki tipler aynı int + int parantezde oldugu için toplandı.
+            Console.ReadLine();
+        }
+    }
+}
