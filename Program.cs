3ed6a48cd6f49782c23c20c877a177b38c5f48b6
Osman Yıldız <67626189+OsmanYildiz1@users.noreply.github.com>
2022-07-15 17:38:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace fordongu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int j = 1; j < 20; j+=2)
+            {
+                Console.WriteLine(j);
+            }
+
+            string[] siparisNo = {"google", "google2", "google3"};
+            for (int i = siparisNo.Length-1 ; i >= 0; i--)
+            {
+                Console.WriteLine(siparisNo[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
