e1c039fb155969822c54811f304c6c6951b2e074
Osman Yıldız <67626189+OsmanYildiz1@users.noreply.github.com>
2022-07-15 17:40:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace dowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 0;                           // DO WHİLE ÖRNEĞİ
+            //do
+            //{
+            //    sayi++;
+            //    if (sayi == 7) continue; // continue bulunduğu yerden sonraki kodu es geçer
+            //    Console.WriteLine(sayi);
+                
+            //    // break; anında döngüyü durdurur.
+              
+
+            //} while (sayi <10);
+                
+            while (sayi<10)
+            {
+                sayi++;
+                if (sayi == 8) continue;
+                Console.WriteLine(sayi);
+              
+            }
+
+
+           
+            Console.ReadLine();
+        }
+    }
+}
